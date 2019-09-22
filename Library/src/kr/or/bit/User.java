98141c17164b90973281cc83f7409a7f2da6f401
edu33 <edu33@DESKTOP-76BJGJA>
2019-09-22 22:20:24 +0900

    Comment fix

--- Library/src/kr/or/bit/User.java
+++ Library/src/kr/or/bit/User.java
@@ -4,44 +4,44 @@ import java.util.Date;
 import java.util.HashMap;
 
 public class User implements Serializable {
-   private String cellNum;
-   private String name;
-   private Date date;
-   private HashMap<Integer, String> borrowBook = null;
+   private String cellNum; // 전화번호
+   private String name; // 이름
+   private Date date; // 시간
+   private HashMap<Integer, String> borrowBook = null; // 빌린 책 목록
    
-   public Date getDate() {
+   public Date getDate() { // date getter
 		return date;
 	}
-	public User() {
+	public User() { // constructor
        super();
        date = new Date();
    }
 	
-   public User(String cellNum, String name) {
+   public User(String cellNum, String name) { // constructor
        super();
        this.cellNum = cellNum;
        this.name = name;
    }
-   public String getName() {
+   public String getName() { // name getter
        return name;
    }
-   public void setName(String name) {
+   public void setName(String name) { // name setter
        this.name = name;
    }
-   public String getCellNum() {
+   public String getCellNum() { // cellnum getter
        return cellNum;
    }
-   public void setCellNum(String cellNum) {
+   public void setCellNum(String cellNum) { // cellnum setter
        this.cellNum = cellNum;
    }
-   public HashMap<Integer, String> getBorrowBook() {
+   public HashMap<Integer, String> getBorrowBook() { // getter borrowbook
        if (borrowBook == null) {
            borrowBook = new HashMap<Integer, String>();
        }
        return borrowBook;
    }
    @Override
-  	public String toString() {
+  	public String toString() { // tostring
   		return "User [cellNum=" + cellNum + ", name=" + name + "]";
   	}
 }
